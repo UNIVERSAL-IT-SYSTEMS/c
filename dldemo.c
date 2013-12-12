f1a822c0251ad98a16f6ab6769d1592fec845aad
Paul Duan <njdp2012@gmail.com>
2013-12-12 12:31:27 +0800

    del

--- dldemo.c
+++
@@ -1,9 +0,0 @@
-#include <dlfcn.h>
-
-main()
-{
-	void *handle=dlopen("./libdemo2.so",RTLD_LAZY);
-	void(*fun)(int)=dlsym(handle,"diamond");
-	fun(5);
-	dlclose(handle);
-}
--- main.c
+++
@@ -1,5 +0,0 @@
-main()
-{
-	int r=inputInt("输入菱形半径：");
-	diamond(r);
-}

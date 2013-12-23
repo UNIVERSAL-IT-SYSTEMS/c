ccc135dcb0b53eb98ff7b9038369324f2526c265
Paul Duan <njdp2012@gmail.com>
2013-12-23 13:29:14 +0800

    1

--- linux/process/ps_execl.c
+++ linux/process/ps_execl.c
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
 {
 	printf("tag\n");//puts无缓冲，不用fflush
 	fflush(stdout);
+	//int r=execl("/bin/ls","arg0","-l",NULL);
 	int r=execlp("ls","lss","-l",(char*)NULL);
 	//printf("return:%d\n",r);
 	return 0;
